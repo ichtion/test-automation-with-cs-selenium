24b4b53eb90d97c88c4b78d4a443ef4fca1f3982
kir <remigiusz_dudek@tlen.pl>
2014-05-09 09:01:42 +0200

    created login Selenium test

--- src/test/java/selenium/SimpleTest.java
+++ src/test/java/selenium/SimpleTest.java
@@ -1,4 +1,125 @@
 package selenium;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class SimpleTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "https://training.bananascrum.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void shouldLoginAsAdminWithAppropriateCredentials() throws Exception {
+        //given
+        openLoginPage();
+
+        //when
+        loginWithCorrectAdminCredentials();
+
+        //then
+        assertThatYouAreLoggedInAsAdmin();
+    }
+
+    @Test
+    public void shouldNotLoginWithNotAppropriateCredentials() throws Exception {
+        //given
+        openLoginPage();
+
+        //when
+        loginWithNotCorrectCredentials();
+
+        //then
+        assertThatYouAreNotLoggedInProperly();
+    }
+
+    private void assertThatYouAreLoggedInAsAdmin() {
+        assertEquals("Admin", driver.findElement(By.id("admin")).getText());
+    }
+
+    private void assertThatYouAreNotLoggedInProperly() {
+        assertEquals("Login failed", driver.findElement(By.id("flash")).getText());
+    }
+
+    private void openLoginPage() {
+        driver.get(baseUrl + "login");
+    }
+
+    private void loginWithCorrectAdminCredentials() {
+        typeTextIntoAField("admin", By.id("login"));
+        typeTextIntoAField("password", By.id("password"));
+        clickElement(By.name("commit"));
+    }
+
+    private void loginWithNotCorrectCredentials() {
+        typeTextIntoAField("asdfahsdgfkjas", By.id("login"));
+        typeTextIntoAField("password", By.id("password"));
+        clickElement(By.name("commit"));
+    }
+
+    private void clickElement(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    private void typeTextIntoAField(String text, By locator) {
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(text);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
 }
