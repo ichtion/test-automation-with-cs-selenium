2c1be0840b5b798155afdc413d695d2cb65a9687
unknown <lesiakowski@plkrnb026.transporeon.nil>
2014-05-09 16:09:04 +0200

    add item

--- src/test/java/selenium/BacklogPage.java
+++ src/test/java/selenium/BacklogPage.java
@@ -4,8 +4,13 @@ import static org.junit.Assert.assertEquals;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static java.lang.System.out;
 
 public class BacklogPage extends Page {
+	
+	double random = Math.random();
 	
 	public BacklogPage(WebDriver driver) {
 		super(driver);
@@ -14,6 +19,21 @@ public class BacklogPage extends Page {
 	public boolean loggedIn() {
 		
 		return (driver.findElement(By.id("admin")).getText() == "admin");
+	}
+
+	public void addItem() {
+
+		driver.findElement(By.linkText("Add item")).click();
+		driver.findElement(By.name("item[user_story]")).sendKeys("pawel_item_automat" + random);
+		driver.findElement(By.name("commit")).click();
+		
+	}
+
+	public boolean itemAdded() {
+		String title = "pawel_item_automat";
+		WebElement item = driver.findElement(By.xpath("//div[text()='" + title + "']"));
+		return true;
+		
 	}
 
 }
