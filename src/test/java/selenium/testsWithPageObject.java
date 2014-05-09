4c118ee9a059a87b69ccafb5d640ca40bc56f5a5
unknown <lesiakowski@plkrnb026.transporeon.nil>
2014-05-09 16:55:20 +0200

    inports

--- src/test/java/selenium/testsWithPageObject.java
+++ src/test/java/selenium/testsWithPageObject.java
@@ -3,6 +3,7 @@ package selenium;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import junitparams.JUnitParamsRunner;
@@ -18,25 +19,33 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import static org.openqa.selenium.remote.DesiredCapabilities.*;
 
 @RunWith(JUnitParamsRunner.class)
 public class testsWithPageObject {
-	private WebDriver driver;
 	private String baseUrl;
 	private boolean acceptNextAlert = true;
 	private StringBuffer verificationErrors = new StringBuffer();
+	
+	protected WebDriver driver;
+	protected ThreadLocal<WebDriver> threadLocalWebDriver = 
+	new ThreadLocal<WebDriver>();
 
 	@Before
 	public void setUp() throws Exception {
-		driver = new FirefoxDriver();
-		baseUrl = "https://training.bananascrum.com/";
+		threadLocalWebDriver.set(new RemoteWebDriver(new URL(
+				"http://localhost:6666/wd/hub"), new DesiredCapabilities(
+				firefox(), chrome(), internetExplorer())));
+		driver = threadLocalWebDriver.get();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
-	@Test	
+	@Test
 	public void shouldLogInWithCorrectCredentials() {
-		
-		//given
+
+		// given
 		LoginPage loginPage = new LoginPage(driver);
 		loginPage.openLoginPage();
 
@@ -46,26 +55,25 @@ public class testsWithPageObject {
 		// then
 		backlogPage.loggedIn();
 	}
-	
+
 	@Test
 	@Parameters({ "Admin, password", "admin, bad_password",
-		"bad_login, password", })
+			"bad_login, password", })
 	public void shouldNotLoginWithIncorrectCredentials(String login,
 			String password) {
-		
-		//given
+
+		// given
 		LoginPage loginPage = new LoginPage(driver);
 		loginPage.openLoginPage();
 
 		// when
-		
+
 		loginPage = loginPage.loginWithInorrectCredentials(login, password);
 
 		// then
 		loginPage.notLoggedIn();
-		
-	}
 
+	}
 
 	@After
 	public void tearDown() throws Exception {
